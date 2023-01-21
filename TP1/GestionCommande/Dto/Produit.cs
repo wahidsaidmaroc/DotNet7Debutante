a647fb00bc5da201f58dd9ddd28d3561555ac914
Said WAHID <said@oumdin.com>
2023-01-21 10:56:24 +0100

    Create Class Dto

---
+++ TP1/GestionCommande/Dto/Produit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto
+{
+    internal class Produit
+    {
+        public int Id { get; set; }
+        public string NomProduit { get; set; } = string.Empty;
+    }
+}
